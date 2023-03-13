8a8b6a0f7da805dd0bf98279b313e76b8fcfc57b
Ali200119 <aliit@code.edu.az>
2023-03-14 00:33:59 +0400

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+"use strict"
+
+let users = [
+    {
+        name: "Mubariz",
+        surname: "Agayev",
+        age: 18
+    },
+
+    {
+        name: "Agshin",
+        surname: "Hummetov",
+        age: 27
+    },
+
+    {
+        name: "Mirze",
+        surname: "Bashirzade",
+        age: 27
+    },
+    
+    {
+        name: "Pervin",
+        surname: "Rehimli",
+        age: 26
+    },
+
+    {
+        name: "Ibrahim",
+        surname: "Akberov",
+        age: 20
+    },
+]
