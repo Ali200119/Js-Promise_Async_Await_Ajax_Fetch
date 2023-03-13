b79203d943badffe594f6cdd3ab6be50fce7c7ea
Ali200119 <aliit@code.edu.az>
2023-03-14 01:20:14 +0400

    feat: setTimeout

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,7 +18,7 @@ let users = [
         surname: "Bashirzade",
         age: 27
     },
-    
+
     {
         name: "Pervin",
         surname: "Rehimli",
@@ -30,4 +30,41 @@ let users = [
         surname: "Akberov",
         age: 20
     },
-]
+];
+
+// function getUsersByName(str) {
+//     let result = users.filter(u => u.name.includes(str));
+
+//     setTimeout(() => {
+//         console.log(result);
+//     }, 3000);
+// }
+
+// getUsersByName("r");
+
+// console.log(users);
+
+let ul = document.querySelector("ul");
+
+function getUsers() {
+    setTimeout(() => {
+        let str = "";
+        
+        for (const user of users) {
+            str += `<li class="list-group-item"><b>${user.name} ${user.surname} ${user.age}</b></li>`
+        }
+
+        ul.innerHTML = str;
+    }, 2000);
+}
+
+// getUsers();
+
+function createUser(user, callback) {
+    setTimeout(() => {
+        users.push(user);
+        callback();
+    }, 3000);
+}
+
+createUser({name: "Elcan", surname: "Qurbanov", age: 20}, getUsers);
